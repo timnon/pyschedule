43a11616e302eba6f15a5aab14b8704d5732be9f
Tim Nonner <tim@nonner.de>
2015-05-10 22:31:43 +0200

    list schedule support

---
+++ src/pyschedule/solvers/listsched.py
@@ -0,0 +1,110 @@
+#! /usr/bin/env python
+from __future__ import print_function
+
+'''
+Copyright 2015 Tim Nonner
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+'''
+
+import copy
+
+def sort_with_precs(scenario) :
+	"""
+	returns the tasks of the given scenario sorted according to the
+	lax precedence constraints and next according to the task length,
+	where large tasks have priority
+	"""
+	S = scenario
+
+	'''
+	# get direct children for top sort
+	children = { T : set() for T in S.tasks() }
+	for P in S.precs_lax() :
+		children[P.left].add(P.right)
+	# extend children to other descendents
+	for T in S.tasks() :
+		task_stack = {T}
+		while task_stack :
+			T_ = task_stack.pop()
+			children[T] = children[T] | children[T_]
+			task_stack = task_stack | children[T_]
+	'''
+
+	import networkx as nx
+	G = nx.DiGraph()
+	G.add_nodes_from(S.tasks())
+	G.add_edges_from([ (P.left,P.right) for P in S.precs_lax() ])
+	task_list = nx.algorithms.topological_sort(G)
+	return task_list
+
+
+
+def solve(scenario,solve_method,sort_method=sort_with_precs,batch_length=1,copy_scenario=False,msg=0) :
+	"""
+	Iteratively adds tasks and uses solve_method to integrate these
+	tasks into the schedule.
+
+	Arguments:
+		scenario: the scenario to solve
+		solve_method : the solve method to integrate new tasks
+		chunk_size : the number of jobs to integrate at a time
+	"""
+
+	S = scenario
+	if copy_scenario :
+		S = copy.deepcopy(scenario)
+
+	task_list = sort_method(S)
+
+	# TODO: add objective adding
+	constraints = S.constraints # keep references and clear old reference list
+	S.constraints = []
+	S.T.clear()
+
+	def batches(tasks, batch_length):
+	    for i in xrange(0, len(tasks), batch_length):
+		yield tasks[i:i+batch_length]
+	
+	for batch in batches(task_list,batch_length) :
+		if msg :
+			print('INFO: batch for list scheduling '+','.join([ str(T) for T in batch]))
+		for T in batch :
+			S.T[T.name] = T
+		S.constraints = [ C for C in constraints if set(C.tasks()).issubset(set(S.tasks())) ]
+		# generate objective
+		solve_method(S)
+		# remove objective
+		S.objective.clear()
+		del S.T['MakeSpan']
+		del S.R['MakeSpan']
+		'''
+		import pyschedule
+		pyschedule.plotters.matplotlib.plot(S)
+		'''
+		
+
+
+		
+		
+
+		
+
+	
+
+
+
