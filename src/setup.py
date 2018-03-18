2295b22c41e63636a4c9f1037cf9dc029b27706e
Tim Nonner <tim@nonner.de>
2018-03-18 13:43:21 +0100

    new version in pypi with reward-functionality and coin support

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyschedule',
-    version='0.2.16',
+    version='0.2.17',
     description='A python package to formulate and solve resource-constrained scheduling problems',
     url='https://github.com/timnon/pyschedule',
     author='Tim Nonner',
