0104f4e40fcede2989a0fcb09d0d98f6f7864720
Andi Meier <andi@daemeier-clan.ch>
2020-11-08 12:22:47 +0100

    examples: umm2019_saturday_002.py...

--- examples/umm2019_saturday_002.py
+++ examples/umm2019_saturday_002.py
@@ -17,93 +17,99 @@ disziplinen = {
                         "Kugel": 'Kugel',
                         "600m": 'Läufe',
                 },
+                "Pause": 10,
                 "Color": "orange",
         },
-        "U16W_5K": {
-                "Disziplinen": {
-                        "80m": 'Läufe',
-                        "Weit": 'Weit',
-                        "Kugel": 'Kugel',
-                        "Hoch": 'Hoch',
-                        "1000m": 'Läufe',
-                },
-                "Color": "pink",
-        },
-        "WOM_7K": {
-                "Disziplinen": {
-                        "100mHü": 'Läufe',
-                        "Hoch": 'Hoch',
-                        "Kugel": 'Kugel',
-                        "200m": 'Läufe',
-                },
-                "Color": "lightgreen",
-        },
-        "U12M_4K": {
-                "Disziplinen": {
-                        "60m": 'Läufe',
-                        "Weit": 'Weit',
-                        "Kugel": 'Kugel',
-                        "600m": 'Läufe',
-                },
-                "Color": "yellow",
-        },
-        "U16M_6K": {
-                "Disziplinen": {
-                        "100mHü": 'Läufe',
-                        "Weit": 'Weit',
-                        "Kugel": 'Kugel',
-                        "Hoch": 'Hoch',
-                        "Diskus": 'Diskus',
-                        "1000m": 'Läufe',
-                },
-                "Color": "lightblue",
-        },
-        "MAN_10K": {
-                "Disziplinen": {
-                        "100m": 'Läufe',
-                        "Weit": 'Weit',
-                        "Kugel": 'Kugel',
-                        "Hoch": 'Hoch',
-                        "400m": 'Läufe',
-                },
-                "Color": "red",
-        },
+        #"U16W_5K": {
+        #        "Disziplinen": {
+        #                "80m": 'Läufe',
+        #                "Weit": 'Weit',
+        #                "Kugel": 'Kugel',
+        #                "Hoch": 'Hoch',
+        #                "1000m": 'Läufe',
+        #        },
+        #        "Pause": 30,
+        #        "Color": "pink",
+        #},
+        #"WOM_7K": {
+        #        "Disziplinen": {
+        #                "100mHü": 'Läufe',
+        #                "Hoch": 'Hoch',
+        #                "Kugel": 'Kugel',
+        #                "200m": 'Läufe',
+        #        },
+        #        "Pause": 30,
+        #        "Color": "lightgreen",
+        #},
+        #"U12M_4K": {
+        #        "Disziplinen": {
+        #                "60m": 'Läufe',
+        #                "Weit": 'Weit',
+        #                "Kugel": 'Kugel',
+        #                "600m": 'Läufe',
+        #        },
+        #        "Pause": 10,
+        #        "Color": "yellow",
+        #},
+        #"U16M_6K": {
+        #        "Disziplinen": {
+        #                "100mHü": 'Läufe',
+        #                "Weit": 'Weit',
+        #                "Kugel": 'Kugel',
+        #                "Hoch": 'Hoch',
+        #                "Diskus": 'Diskus',
+        #                "1000m": 'Läufe',
+        #        },
+        #        "Pause": 30,
+        #        "Color": "lightblue",
+        #},
+        #"MAN_10K": {
+        #        "Disziplinen": {
+        #                "100m": 'Läufe',
+        #                "Weit": 'Weit',
+        #                "Kugel": 'Kugel',
+        #                "Hoch": 'Hoch',
+        #                "400m": 'Läufe',
+        #        },
+        #        "Pause": 30,
+        #        "Color": "red",
+        #},
 }
 
 teilnehmer = {
         "U12W_4K": {
                 "Gr14": 13,
-                "Gr15": 12,
-                "Gr16": 12,
-                "Gr17": 12,
-                "Gr18": 12,
-                "Gr19": 12,
-                "Gr20": 12,
+                #"Gr15": 12,
+                #"Gr16": 12,
+                #"Gr17": 12,
+                #"Gr18": 12,
+                #"Gr19": 12,
+                #"Gr20": 12,
         },
-        "U16W_5K": {
-                "Gr3": 10,
-                "Gr4": 10,
-                "Gr5": 11,
-                "Gr6": 11,
-        },
-        "WOM_7K": {
-                "Gr1": 11,
-                "Gr2": 14,
-        },
-        "U12M_4K": {
-                "Gr30": 12,
-                "Gr31": 13,
-                "Gr32": 11,
-                "Gr33": 12,
-                "Gr34": 11,
-        },
-        "U16M_6K": {
-                "Gr24": 11,
-                "Gr25": 10,
-        },
-        "MAN_10K": {
-                "Gr23": 9,
-        },
+        #"U16W_5K": {
+        #        "Gr3": 10,
+        #        "Gr4": 10,
+        #        "Gr5": 11,
+        #        "Gr6": 11,
+        #},
+        #"WOM_7K": {
+        #        "Gr1": 11,
+        #        "Gr2": 14,
+        #},
+        #"U12M_4K": {
+        #        "Gr30": 12,
+        #        "Gr31": 13,
+        #        "Gr32": 11,
+        #        "Gr33": 12,
+        #        "Gr34": 11,
+        #},
+        #"U16M_6K": {
+        #        "Gr24": 11,
+        #        "Gr25": 10,
+        #},
+        #"MAN_10K": {
+        #        "Gr23": 9,
+        #},
 }
 
 anlagen = {
@@ -134,6 +140,7 @@ print("tasks:")
 for wettkampf_name, wettkampf in disziplinen.items():
         disziplinen = wettkampf["Disziplinen"]
         color = wettkampf["Color"]
+        pause_in_minutes = wettkampf["Pause"]
         first_group_name = next(iter(teilnehmer[wettkampf_name]))
         for group_name, num_group_members in teilnehmer[wettkampf_name].items():
                 tasks = []
@@ -151,15 +158,19 @@ for wettkampf_name, wettkampf in disziplinen.items():
                             task[task_name] = t
                 task_iter = iter(tasks)
                 current_task = next(task_iter)
+                num_pause_units = pause_in_minutes / unit_in_minutes
+                print("num_pause_units={}".format(num_pause_units))
                 for next_task in task_iter:
-                        scenario += current_task + 2 < next_task
+                        print("current-task: {}, nest-task: {}".format(current_task, next_task))
+                        scenario += current_task + num_pause_units < next_task
                         current_task = next_task
                 if group_name != first_group_name:
-                        scenario += current_task + 2 < task[last_task_name]
+                        print("current-task: {}, last-task: {}".format(current_task, task[last_task_name]))
+                        scenario += current_task + num_pause_units < task[last_task_name]
 
 print("scenario={}".format(scenario))
 
-if solvers.mip.solve(scenario, time_limit=600, msg=1):
+if solvers.mip.solve(scenario, time_limit=600, msg=0):
     print(scenario.solution())
     plotters.matplotlib.plot(scenario, show_task_labels=True, img_filename='tmp.png', fig_size=(100,10))
 else:
