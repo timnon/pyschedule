e756d8f77da541cd9ea4cef8c503de8a8e145e0c
Andi Meier <andi@daemeier-clan.ch>
2020-11-24 12:59:42 +0100

    examples: umm2019: add argument '--random-seed'

--- examples/umm/umm2019.py
+++ examples/umm/umm2019.py
@@ -13,12 +13,15 @@ parser.add_argument('--print-scenario-and-exit', action="store_true",
 default_arguments = {
     "time_limit": "10m",
     "ratio_gap": 0.0,
+    "random_seed": None,
 }
 help_text = 'time limit, e.g. 30s, 10m, 1h (default: {})'.format(default_arguments["time_limit"])
 parser.add_argument('-v', '--verbose', action="store_true", help="be verbose")
 parser.add_argument('--time-limit', default=default_arguments["time_limit"], help=help_text)
 help_text = 'ratio gap, e.g. 0.3 (default: {})'.format(default_arguments["ratio_gap"])
 parser.add_argument('--ratio-gap', type=float, default=default_arguments["ratio_gap"], help=help_text)
+help_text = 'random seed, e.g. 42 (default: {})'.format(default_arguments["random_seed"])
+parser.add_argument('--random-seed', type=int, default=default_arguments["random_seed"], help=help_text)
 parser.add_argument('--dont-set-start-time', action="store_true", help="don't set start time")
 valid_wettkampf_days = ['saturday', 'sunday']
 parser.add_argument('day', type=str.lower, choices=valid_wettkampf_days, help='wettkampf day')
@@ -349,4 +352,4 @@ if args.print_scenario_and_exit:
     logging.info("scenario: {}".format(scenario_as_string))
     sys.exit()
 logging.debug("scenario: {}".format(scenario_as_string))
-event.solve(time_limit=args.time_limit, ratio_gap=args.ratio_gap)
+event.solve(time_limit=args.time_limit, ratio_gap=args.ratio_gap, random_seed=args.random_seed)
