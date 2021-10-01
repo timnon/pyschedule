7ed3ee744ce39269f73a7973cd22c81cf5ce5e3d
Andi Meier <andi@daemeier-clan.ch>
2021-10-01 17:09:30 +0200

    examples/umm: have borders for all table cells

--- examples/umm/zeitplan_xlsx_writer.py
+++ examples/umm/zeitplan_xlsx_writer.py
@@ -8,30 +8,30 @@ class Zeitplan(object):
         "U12W": "orange",
         "U12M": "yellow",
         "U16W": "pink",
-        "U16M": "blue",
+        "U16M": "cyan",
         "WOM_7K": "green",
         "MAN_10K": "red",
         # Sunday:
         "U14W": "orange",
         "U14M": "yellow",
         "WOM_5K": "pink",
-        "MAN_6K": "blue",
+        "MAN_6K": "cyan",
         "WOM_7K": "green",
         "MAN_10K": "red",
     }
 
-    def __init__(self, elements, resources):
-        self._elements = elements
+    def __init__(self, tasks, resources):
+        self._tasks = tasks
         self._resources = resources
 
-    def getElements(self, resource_name):
-        elements = []
+    def getTasks(self, resource_name):
+        tasks = []
         column_index = self._resources.index(resource_name)
-        for element in self._elements:
-            if element[1] == resource_name:
-                print(column_index, element, self.getColorFromEvent(element[0]))
-                elements.append((element, column_index, self.getColorFromEvent(element[0])))
-        return elements
+        for task in self._tasks:
+            if task[1] == resource_name:
+                print(column_index, task, self.getColorFromEvent(task[0]))
+                tasks.append((task, column_index, self.getColorFromEvent(task[0])))
+        return tasks
 
     def getColorFromEvent(self, eventName):
         for k, v in self._eventToColorMapping.items():
@@ -47,31 +47,28 @@ def main(args):
 
     withoutBrackets = contentAsString[1:-1]
     #print("without-brackets: {!r}".format(withoutBrackets))
-    elementsAsStrings = withoutBrackets.split("), (")
-    elementsAsStrings[0] = elementsAsStrings[0][1:]
-    elementsAsStrings[-1] = elementsAsStrings[-1][:-1]
-    #print("elements: {!r}".format(elementsAsStrings))
-    elements = []
-    for elementAsString in elementsAsStrings:
-        parts = elementAsString.split(', ')
-        elements.append((parts[0], parts[1], int(parts[2]), int(parts[3])))
-    #print("elements: {!r}".format(elements))
+    tasksAsStrings = withoutBrackets.split("), (")
+    tasksAsStrings[0] = tasksAsStrings[0][1:]
+    tasksAsStrings[-1] = tasksAsStrings[-1][:-1]
+    #print("tasks: {!r}".format(tasksAsStrings))
+    tasks = []
+    for taskAsString in tasksAsStrings:
+        parts = taskAsString.split(', ')
+        tasks.append((parts[0], parts[1], int(parts[2]), int(parts[3])))
+    #print("tasks: {!r}".format(tasks))
 
-    resources = ["Läufe", "Weit1", "Weit2", "Hoch1", "Hoch2", "Kugel1", "Kugel2", "Diskus", "Speer1", "Speer2", "Stab"]
+    resources = ["Läufe", "Weit1", "Weit2", "Hoch1", "Hoch2", "Kugel1", "Kugel2", "Diskus", "Speer", "Stab"]
 
-    zeitplan = Zeitplan(elements, resources)
-
-    lauf_elements = zeitplan.getElements("Läufe")
-    print("lauf-elements: {!r}".format(lauf_elements))
+    zeitplan = Zeitplan(tasks, resources)
 
     writer = pandas.ExcelWriter('zeitplan.xlsx', engine='xlsxwriter')
     workbook = writer.book
-    worksheet = workbook.add_worksheet('Samstag')
+    worksheet = workbook.add_worksheet()
 
     title_cell_format = workbook.add_format()
     title_cell_format.set_font_size(14)
     title_cell_format.set_bold()
-    worksheet.write('A1', 'Zeitplan Uster Mehrkampf Meeting: Samstag 25.9.2021', title_cell_format)
+    worksheet.write('A1', 'Zeitplan Uster Mehrkampf Meeting', title_cell_format)
 
     heading_cell_format = workbook.add_format()
     heading_cell_format.set_bold()
@@ -81,12 +78,19 @@ def main(args):
     empty_table_cell_format.set_border()
 
     for column_index, resource in enumerate(resources):
-        elements = zeitplan.getElements(resource)
-        for element in elements:
+        tasks = zeitplan.getTasks(resource)
+        if resource == "Läufe":
+            first_task = tasks[0][0][2]
+            last_task = tasks[-1][0][3] - 2
+            worksheet.write(2, 0, 'Zeit', heading_cell_format)
+            for row_index in range(first_task, last_task + 1):
+                worksheet.write(row_index + 3, 0, str(row_index), empty_table_cell_format)
+        filled_slots = []
+        for task in tasks:
             worksheet.write(2, column_index + 1, resource, heading_cell_format)
-            color = element[2]
-            start_time = element[0][2]
-            end_time = element[0][3]
+            color = task[2]
+            start_time = task[0][2]
+            end_time = task[0][3]
             cell_format = workbook.add_format()
             cell_format.set_pattern(1)
             cell_format.set_border()
@@ -94,11 +98,14 @@ def main(args):
             for row_index in range(start_time, end_time - 1):
                 content = ''
                 if row_index == start_time:
-                    content = element[0][0]
+                    content = task[0][0]
                     if resource != "Läufe":
                         content = content.split('_')[-2]
                 worksheet.write(row_index + 3, column_index + 1, content, cell_format)
-
+                filled_slots.append(row_index)
+        for row_index in range(first_task, last_task + 1):
+            if row_index not in filled_slots:
+                worksheet.write(row_index + 3, column_index + 1, '', empty_table_cell_format)
     writer.save()
 
 
